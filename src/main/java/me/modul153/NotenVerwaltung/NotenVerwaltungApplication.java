898644a8f7be707929784077cdb3af37a14f8423
McTNTCrafter // luca <luca.guettinger@gmail.com>
2019-08-27 18:30:01 +0200

    initial commit

---
+++ src/main/java/me/modul153/NotenVerwaltung/NotenVerwaltungApplication.java
@@ -0,0 +1,13 @@
+package me.modul153.NotenVerwaltung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotenVerwaltungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NotenVerwaltungApplication.class, args);
+	}
+
+}
