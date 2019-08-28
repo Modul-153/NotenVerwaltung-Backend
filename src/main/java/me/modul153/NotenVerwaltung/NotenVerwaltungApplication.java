ac9291003793a2a27b012f3d018ef44bfabb3463
LucaGuettinger <luca.guettinger@cmiag.ch>
2019-08-28 11:01:18 +0200

    add viable jar to services and only start services nessesary

--- src/main/java/me/modul153/NotenVerwaltung/NotenVerwaltungApplication.java
+++ src/main/java/me/modul153/NotenVerwaltung/NotenVerwaltungApplication.java
@@ -3,7 +3,9 @@ package me.modul153.NotenVerwaltung;
 import net.myplayplanet.services.ServiceCluster;
 import net.myplayplanet.services.config.ConfigService;
 import net.myplayplanet.services.connection.ConnectionService;
+import net.myplayplanet.services.logger.Logger;
 import net.myplayplanet.services.logger.LoggerService;
+import net.myplayplanet.services.logger.sinks.MockSink;
 import net.myplayplanet.services.logger.sinks.MySQLSink;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -14,10 +16,9 @@ import java.io.File;
 public class NotenVerwaltungApplication {
 
 	public NotenVerwaltungApplication() {
-		MySQLSink.setDisable(true);
-		ServiceCluster.addServices(true, new LoggerService());
 		File configPath = new File("C:\\temp\\nv-backend");
-		configPath.mkdirs();
+		LoggerService.setDefaultSink(new MockSink());
+		ServiceCluster.addServices(true, new LoggerService());
 		ServiceCluster.addServices(true, new ConfigService(configPath));
 		ServiceCluster.addServices(true, new ConnectionService());
 	}
