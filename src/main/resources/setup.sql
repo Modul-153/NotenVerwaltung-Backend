448eb9b72fbc09c7fc6c4d6b0a4c7cf8d5a8c848
LucaGuettinger <luca.guettinger@cmiag.ch>
2019-10-01 13:19:40 +0200

    change script

--- src/main/resources/setup.sql
+++ src/main/resources/setup.sql
@@ -12,8 +12,7 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`city`
     `city_id` SERIAL,
     `name`    VARCHAR(45) NOT NULL,
     `zipcode` INT         NOT NULL,
-    PRIMARY KEY (`city_id`),
-    UNIQUE INDEX `city_id_UNIQUE` (`city_id` ASC)
+    PRIMARY KEY (`city_id`)
 )
     ENGINE = InnoDB;
 
@@ -24,7 +23,7 @@ create table if not exists `notenverwaltung`.`subject`
 (
     `subject_id` SERIAL,
     `name`       varchar(50) not null,
-    unique index `subject_id_UNIQUE` (`subject_id` ASC)
+    primary key (subject_id)
 )
     ENGINE = InnoDB;
 
@@ -40,10 +39,8 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`user`
     `number`    INT             NOT NULL,
     `street`    VARCHAR(45)     NOT NULL,
     `city_id`   BIGINT UNSIGNED NOT NULL,
-    `password` VARCHAR(1000)    NOT NULL,
+    `password`  VARCHAR(1000)   NOT NULL,
     PRIMARY KEY (`user_id`),
-    UNIQUE INDEX `idPersonTable_UNIQUE` (`user_id` ASC),
-    INDEX `adress_id_idx` (`city_id` ASC),
     CONSTRAINT `fk_city_id_user`
         FOREIGN KEY (`city_id`)
             REFERENCES `notenverwaltung`.`city` (`city_id`)
@@ -63,7 +60,6 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`school`
     `street`     VARCHAR(45)     NOT NULL,
     `city_id`    BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (`school_id`),
-    INDEX `city_id_idx` (`city_id` ASC),
     CONSTRAINT `fk_city_id_school`
         FOREIGN KEY (`city_id`)
             REFERENCES `notenverwaltung`.`city` (`city_id`)
@@ -72,44 +68,6 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`school`
 )
     ENGINE = InnoDB;
 
--- -----------------------------------------------------
--- Table `notenverwaltung`.`class`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `notenverwaltung`.`class`
-(
-    `class_id`  SERIAL,
-    `name`      VARCHAR(45)     NOT NULL,
-    `school_id` BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (`class_id`),
-    INDEX `school_id_idx` (`school_id` ASC),
-    CONSTRAINT `fk_school_id_class`
-        FOREIGN KEY (`school_id`)
-            REFERENCES `notenverwaltung`.`school` (`school_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
-
-
--- -----------------------------------------------------
--- Table `notenverwaltung`.`exam`
--- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `notenverwaltung`.`exam`
-(
-    `exam_id` SERIAL,
-    `mark`    INT             NOT NULL,
-    `date`    DATE            NOT NULL,
-    `user_id` BIGINT UNSIGNED NOT NULL,
-    PRIMARY KEY (`exam_id`),
-    UNIQUE INDEX `exam_id_UNIQUE` (`exam_id` ASC),
-    INDEX `user_id_idx` (`user_id` ASC),
-    CONSTRAINT `fk_user_id_exam`
-        FOREIGN KEY (`user_id`)
-            REFERENCES `notenverwaltung`.`user` (`user_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION
-)
-    ENGINE = InnoDB;
 
 
 -- -----------------------------------------------------
@@ -120,7 +78,6 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`teacher`
     `teacher_id` SERIAL,
     `user_id`    BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY (`teacher_id`),
-    UNIQUE INDEX `teacher_id_UNIQUE` (`teacher_id` ASC),
     INDEX `user_id_idx` (`user_id` ASC),
     CONSTRAINT `fk_user_id_teacher`
         FOREIGN KEY (`user_id`)
@@ -130,6 +87,28 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`teacher`
 )
     ENGINE = InnoDB;
 
+-- -----------------------------------------------------
+-- Table `notenverwaltung`.`class`
+-- -----------------------------------------------------
+CREATE TABLE IF NOT EXISTS `notenverwaltung`.`class`
+(
+    `class_id`        SERIAL,
+    `name`            VARCHAR(45)     NOT NULL,
+    `school_id`       BIGINT UNSIGNED NOT NULL,
+    `primary_teacher` BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`class_id`),
+    CONSTRAINT `fk_school_id_class`
+        FOREIGN KEY (`school_id`)
+            REFERENCES `notenverwaltung`.`school` (`school_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION,
+    CONSTRAINT `fk_class_primary_teacher`
+        FOREIGN KEY (`primary_teacher`)
+            REFERENCES `notenverwaltung`.`teacher` (`teacher_id`)
+            ON DELETE NO ACTION
+            ON UPDATE NO ACTION
+)
+    ENGINE = InnoDB;
 
 -- -----------------------------------------------------
 -- Table `notenverwaltung`.`student`
@@ -137,10 +116,8 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`teacher`
 CREATE TABLE IF NOT EXISTS `notenverwaltung`.`student`
 (
     `student_id` SERIAL,
-    `user_id`    BIGINT UNSIGNED NOT NULL,
+    `user_id`    BIGINT UNSIGNED NOT NULL unique,
     PRIMARY KEY (`student_id`),
-    UNIQUE INDEX `student_id_UNIQUE` (`student_id` ASC),
-    UNIQUE INDEX `user_id_UNIQUE` (`user_id` ASC),
     CONSTRAINT `fk_user_id_student`
         FOREIGN KEY (`user_id`)
             REFERENCES `notenverwaltung`.`user` (`user_id`)
@@ -157,9 +134,8 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`class_member`
 (
     `student_id` BIGINT UNSIGNED NOT NULL,
     `class_id`   BIGINT UNSIGNED NOT NULL,
-    `from`       date NOT NULL,
-    `to`         date NOT NULL,
-    UNIQUE INDEX `user_id_UNIQUE` (`student_id` ASC),
+    `from`       date            NOT NULL,
+    `to`         date            NOT NULL,
     INDEX `class_id_idx` (`class_id` ASC),
     CONSTRAINT `fk_teacher_id_class_member`
         FOREIGN KEY (`student_id`)
@@ -180,14 +156,14 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`class_member`
 -- -----------------------------------------------------
 CREATE TABLE IF NOT EXISTS `notenverwaltung`.`teacher_class_subject`
 (
+    tcs_id       serial,
     `teacher_id` BIGINT UNSIGNED NOT NULL,
     `class_id`   BIGINT UNSIGNED NOT NULL,
     `subject_id` BIGINT UNSIGNED NOT NULL,
-    `from`       date NOT NULL,
-    `to`         date NOT NULL,
-    INDEX `tcs_class_id_idx` (`class_id` ASC),
-    INDEX `tcs_teacher_id_idx` (`teacher_id` ASC),
-    INDEX `tcs_subject_id_idx` (`subject_id` ASC),
+    `from`       date            NOT NULL,
+    `to`         date            NOT NULL,
+    primary key (tcs_id),
+    constraint u_tcs unique (teacher_id, class_id, subject_id),
     CONSTRAINT `fk_tcs_teacher_id`
         FOREIGN KEY (`teacher_id`)
             REFERENCES `notenverwaltung`.`teacher` (`teacher_id`)
@@ -207,27 +183,31 @@ CREATE TABLE IF NOT EXISTS `notenverwaltung`.`teacher_class_subject`
     ENGINE = InnoDB;
 
 -- -----------------------------------------------------
--- Table `notenverwaltung`.`primary_info`
+-- Table `notenverwaltung`.`exam`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `notenverwaltung`.`primary_info`
+CREATE TABLE IF NOT EXISTS `notenverwaltung`.`exam`
 (
-    `teacher_id`      BIGINT UNSIGNED NOT NULL,
-    `class_id`        BIGINT UNSIGNED NOT NULL,
-    `from`            date NOT NULL,
-    `to`              date NOT NULL,
-    INDEX `primary_info_teacher_id_idx` (`teacher_id` ASC),
-    INDEX `primary_info_class_id_idx` (`class_id` ASC),
-    CONSTRAINT `fk_primary_info_teacher_id`
-        FOREIGN KEY (`teacher_id`)
-            REFERENCES `notenverwaltung`.`teacher` (`teacher_id`)
-            ON DELETE NO ACTION
-            ON UPDATE NO ACTION,
-    CONSTRAINT `fk_primary_info_class_id`
-        FOREIGN KEY (`class_id`)
-            REFERENCES `notenverwaltung`.`class` (`class_id`)
+    `exam_id` SERIAL,
+    `date`    DATE            NOT NULL,
+    `tcs_id`  BIGINT UNSIGNED NOT NULL,
+    PRIMARY KEY (`exam_id`),
+    CONSTRAINT `fk_exam_tcs_id`
+        FOREIGN KEY (`tcs_id`)
+            REFERENCES `teacher_class_subject` (`tcs_id`)
             ON DELETE NO ACTION
             ON UPDATE NO ACTION
 )
     ENGINE = InnoDB;
+
+
+create table `notenverwaltung`.`exam_result`
+(
+    exam_id    bigint unsigned        not null,
+    student_id bigint unsigned        not null,
+    mark       decimal(3, 2) unsigned not null,
+    constraint u_exam_student unique (exam_id, student_id),
+    constraint fk_exam_result_exam foreign key (exam_id) references exam(exam_id),
+    constraint fk_exam_result_student foreign key (student_id) references student(student_id)
+);
 
 COMMIT;
